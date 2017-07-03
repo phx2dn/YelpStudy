c434a38b56097d4ffdae944f89ad03003fb89e31
PHX WANG <cong@CONGde-MacBook-Air.local>
2017-07-03 12:52:25 -0500

    Initial Commit

---
+++ YelpStudy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YelpStudy
+//
+//  Created by PHX WANG on 7/3/17.
+//  Copyright © 2017 PHX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YelpStudy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YelpStudy
+//
+//  Created by PHX WANG on 7/3/17.
+//  Copyright © 2017 PHX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
